f2d13615a9e345551dfda3727cc3ef1d02fb37da
whucs-vegetabledog <79696053+whucs-vegetabledog@users.noreply.github.com>
2021-04-24 15:00:46 +0800

    Add files via upload

---
+++ 6/OrderServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsharpHomework6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpHomework6.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void OrderServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ShowOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void addOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void removeOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void changeOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void searchOrderTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
