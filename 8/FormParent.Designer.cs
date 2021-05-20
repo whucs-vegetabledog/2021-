afdb98aeb6b42e170ec84988edc5c12a6d5efe7b
whucs-vegetabledog <79696053+whucs-vegetabledog@users.noreply.github.com>
2021-05-20 09:03:07 +0800

    Add files via upload

---
+++ 8/FormParent.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace OrderForm {
+  partial class FormParent {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.tabControlMain = new System.Windows.Forms.TabControl();
+      this.MainPage = new System.Windows.Forms.TabPage();
+      this.tabControlMain.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // tabControl1
+      // 
+      this.tabControlMain.Controls.Add(this.MainPage);
+      this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+      this.tabControlMain.Name = "tabControl1";
+      this.tabControlMain.SelectedIndex = 0;
+      this.tabControlMain.Size = new System.Drawing.Size(800, 450);
+      this.tabControlMain.TabIndex = 0;
+      // 
+      // MainPage
+      // 
+      this.MainPage.Location = new System.Drawing.Point(4, 22);
+      this.MainPage.Name = "MainPage";
+      this.MainPage.Padding = new System.Windows.Forms.Padding(3);
+      this.MainPage.Size = new System.Drawing.Size(792, 424);
+      this.MainPage.TabIndex = 0;
+      this.MainPage.Text = "订单管理";
+      this.MainPage.UseVisualStyleBackColor = true;
+      // 
+      // FormParent
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.tabControlMain);
+      this.Name = "FormParent";
+      this.Text = "FormParent";
+      this.Load += new System.EventHandler(this.FormParent_Load);
+      this.tabControlMain.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TabControl tabControlMain;
+    private System.Windows.Forms.TabPage MainPage;
+  }
+}
---
+++ 8/FormParent.cs
@@ -0,0 +1,56 @@
+﻿using OrderApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderForm {
+  public partial class FormParent : Form {
+    FormMain formMain = new FormMain();
+    public FormParent() {
+      InitializeComponent();
+    }
+
+    private void FormParent_Load(object sender, EventArgs e) {
+      formMain.TopLevel = false;
+      formMain.FormBorderStyle = FormBorderStyle.None;
+      formMain.Dock = DockStyle.Fill;
+      formMain.ShowEditForm = this.ShowEditForm;//设置委托
+      this.MainPage.Controls.Add(formMain);
+      formMain.Show();
+    }
+    
+    //显示编辑窗口
+    public void ShowEditForm(FormEdit formEdit) {
+      if (tabControlMain.TabCount > 1) {
+        tabControlMain.TabPages.RemoveAt(1);
+      }
+      //新建标签页
+      TabPage tabPage = new TabPage() { Text = "编辑订单" };
+      tabControlMain.TabPages.Add(tabPage);
+      tabControlMain.SelectTab(1);
+      //在新标签页中显示编辑窗口
+      formEdit.TopLevel = false;
+      formEdit.FormBorderStyle = FormBorderStyle.None;
+      formEdit.Dock = DockStyle.Fill;
+      formEdit.CloseEditFrom = this.CloseEditForm;//设置委托
+      tabPage.Controls.Add(formEdit);
+      formEdit.Show();
+    }
+
+    //关闭编辑窗口
+    public void CloseEditForm(FormEdit formEdit) {
+      if (formEdit != null) formEdit.Close();
+      if (tabControlMain.TabCount > 1) {
+        tabControlMain.TabPages.RemoveAt(1);
+      }
+      formMain.QueryAll();
+
+    }
+  }
+}
