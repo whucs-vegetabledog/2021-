02c5acc0c9f112e7abdd8d9e4bdbdd911cb5f57a
whucs-vegetabledog <79696053+whucs-vegetabledog@users.noreply.github.com>
2021-06-20 15:17:57 +0800

    Add files via upload

---
+++ 11/BloggingContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.EntityFramework;
+
+namespace EFDemo2 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class BloggingContext : DbContext {
+    public BloggingContext() : base("BlogDataBase") {
+      Database.SetInitializer(
+        new DropCreateDatabaseIfModelChanges<BloggingContext>());
+    }
+
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+  }
+}
